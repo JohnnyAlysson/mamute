6f2a07d98c798549cd1fcc6f078d6bbe50fb5246
JohnnyAlysson <johnalysson30@gmail.com>
2024-04-25 11:53:06 -0300

    fix slider

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
 /* Toggle between adding and removing the "responsive" class to menu when the user clicks on the icon */
+
+
 function myFunction() {
     var x = document.getElementById("mymenu");
     if (x.className === "menu") {
